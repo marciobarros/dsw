7dc916a9a21de51a6b05d8240f80e7575746df68
Marcio Barros <marcio.barros@gmail.com>
2024-03-12 18:20:24 -0300

    Insercao e remocao em MySQL

---
+++ NodeJS/14 NodeJS MySQL/02-insercao-registro.js
@@ -0,0 +1,39 @@
+const mysql = require("promise-mysql");
+const fs = require("fs");
+
+/*
+ * Pega a conexao com o banco de dados
+ */ 
+async function getConnection() {
+  let database = await mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "marcio",
+    password: "marcio",
+    database: "cidades"
+  });
+
+  return database;
+}
+
+/*
+ * Programa principal
+ */
+(async () => {
+  try {
+    db = await getConnection();
+    let sql = "INSERT INTO Regioes (nome) VALUES (?)";
+    let data = ["Lunar"];
+    let result = await db.query(sql, data);
+    console.log(result.affectedRows + " linha afetada.");
+    console.log("Identificador do novo registro " + result.insertId);
+    console.log("Avisos: " + result.warningCount + " " + result.message);
+    db.end();
+  } 
+  catch (err) {
+    if (db) {
+      db.end();
+    }
+    console.error(err);
+  } 
+})();
